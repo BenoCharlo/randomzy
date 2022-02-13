0adfc294b9624181aa8d769ccef39ed4cb8d72a9
Charles DOKODJO <dokodjokwassibenocharles@yahoo.fr>
2022-02-13 18:09:27 +0100

    return a message in create and delete folder

--- src/helpers.py
+++ src/helpers.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 
 def create_folder(folder_name: str = "temp"):
-    return Path(folder_name).mkdir(exist_ok=True)
+    Path(folder_name).mkdir(exist_ok=True)
+    return f'"=" * 5 + "Creation of {folder_name} done" + "=" * 5'
 
 
 def delete_folder(folder_name: str = "temp"):
-    return shutil.rmtree(Path(folder_name))
+    shutil.rmtree(Path(folder_name))
+    return f'"=" * 5 + "Deletion of {folder_name} done" + "=" * 5'
