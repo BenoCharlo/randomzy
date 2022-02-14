f788e4feb9ffc10bad6e94fa5a89ff53ecf6d708
Charles DOKODJO <dokodjokwassibenocharles@yahoo.fr>
2022-02-14 18:12:39 +0100

    remove profile and file name definitions

--- src/users.py
+++ src/users.py
@@ -1,23 +1,16 @@
-import datetime
-import random
 from pathlib import Path
 
 import pandas as pd
 from faker import Faker
 from faker.providers import profile
 
-import settings as settings
-
-N_PROFILES = random.randint(settings.MIN_PROFILES, settings.MAX_PROFILES)
-FILENAME = str(datetime.datetime.now()).replace(" ", "_") + ".csv"
-
 
 fake = Faker()
 fake.add_provider(profile)
 
 
-def generate_profiles() -> list:
-    return [fake.profile() for _ in range(N_PROFILES)]
+def generate_profiles(n_profiles: int) -> list:
+    return [fake.profile() for _ in range(n_profiles)]
 
 
 def save_profiles(profiles: list, filepath: Path) -> str:
