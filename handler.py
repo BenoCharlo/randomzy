f013fd32f5c55c1a14bacbff15b84dc9c072b63c
Charles DOKODJO <dokodjokwassibenocharles@yahoo.fr>
2022-02-17 11:21:43 +0100

    replace pathlib paths with os path

--- handler.py
+++ handler.py
@@ -2,7 +2,6 @@ import os
 import datetime
 import random
 
-from pathlib import Path
 import boto3
 
 from src import helpers, users, settings
@@ -15,14 +14,14 @@ FILENAME = str(datetime.datetime.now()).replace(" ", "_") + ".csv"
 def main(event, context):
     helpers.create_folder()
     users.save_profiles(
-        profiles=users.generate_profiles(N_PROFILES), filepath=Path(settings.TEMP_FOLDER, FILENAME)
+        profiles=users.generate_profiles(N_PROFILES), filepath=os.path.join(settings.TEMP_FOLDER, FILENAME)
     )
     access_key = os.getenv("ACCESS_KEY")
     secret_key = os.getenv("SECRET_KEY")
     bucket_name = os.getenv("BUCKET")
 
     s3 = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-    s3.upload_file(str(Path(settings.TEMP_FOLDER, FILENAME)), bucket_name, FILENAME)
+    s3.upload_file(os.path.join(settings.TEMP_FOLDER, FILENAME), bucket_name, FILENAME)
 
     helpers.delete_folder()
 
--- src/helpers.py
+++ src/helpers.py
@@ -1,14 +1,14 @@
 import shutil
-from pathlib import Path
+import os
 
 from . import settings
 
 
 def create_folder(folder_name: str = settings.TEMP_FOLDER):
-    Path(folder_name).mkdir(exist_ok=True)
+    os.makedirs(folder_name, exist_ok=True)
     return f'"=" * 5 + "Creation of {folder_name} done" + "=" * 5'
 
 
 def delete_folder(folder_name: str = settings.TEMP_FOLDER):
-    shutil.rmtree(Path(folder_name))
+    shutil.rmtree(os.path.join(folder_name))
     return f'"=" * 5 + "Deletion of {folder_name} done" + "=" * 5'
