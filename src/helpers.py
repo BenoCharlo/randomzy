b99cb6ade48384159f70224cabbe7fa70a1c6584
Charles DOKODJO <dokodjokwassibenocharles@yahoo.fr>
2022-02-20 15:01:22 +0100

    remove = and +

--- src/helpers.py
+++ src/helpers.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 def create_folder(output_path: str):
     Path(output_path).mkdir(exist_ok=True)
-    print(f'{"=" * 5} + "Creation of {output_path} done" + {"=" * 5}')
+    print(f'{"=" * 5} Creation of {output_path} done {"=" * 5}')
 
 
 def delete_folder(output_path: str):
     shutil.rmtree(Path(output_path))
-    print(f'{"=" * 5} + "Deletion of {output_path} done" + {"=" * 5}')
+    print(f'{"=" * 5} Deletion of {output_path} done {"=" * 5}')
